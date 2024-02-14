c3c868805e523691ab4db63fa522dda55ef0341a
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2024-02-14 12:12:45 +0600

    class card backed logic implimented

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
@@ -30,9 +30,48 @@ async function run() {
       .db("monchobiSchoolDB")
       .collection("classes");
 
+    const enrolledCollection = client
+      .db("monchobiSchoolDB")
+      .collection("enrolled");
+
+    // classes database
     app.get("/classes", async (req, res) => {
       const result = await classesCollection.find().toArray();
       res.send(result);
+    });
+
+    // enrollments database in the monchobi art class
+    app.post("/enrolled/:enrolledId", async (req, res) => {
+      const classId = req.params.enrolledId;
+      // find class id
+      const enrolledClass = await classesCollection.findOne({
+        _id: new ObjectId(classId),
+      });
+      if (!enrolledClass) {
+        return res
+          .status(404)
+          .send({ error: true, message: "Class not found" });
+      }
+      // check if class already exists
+      if (enrolledClass.availableSeat > 0) {
+        await classesCollection.updateOne(
+          { _id: new ObjectId(classId) },
+          { $inc: { availableSeat: -1 } }
+        );
+
+        // save enrolled class
+        await enrolledCollection.insertOne({
+          classId: new ObjectId(classId),
+          enrollmentDate: new Date(),
+        });
+        return res
+          .status(200)
+          .send({ error: false, message: "Enrollment successful" });
+      } else {
+        return res
+          .status(400)
+          .send({ error: true, message: "No seats available" });
+      }
     });
 
     // Send a ping to confirm a successful connection
