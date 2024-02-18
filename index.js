f78fc2301cade5c13c8e16bb98b5746ac36b92d0
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2024-02-18 19:06:31 +0600

    users api done

--- index.js
+++ index.js
@@ -95,12 +95,12 @@ async function run() {
     // save user email and role in DB
     app.put("/users/:email", async (req, res) => {
       const email = req.params.email;
-      const user= req.body;
+      const user = req.body;
       const query = { email: email };
       const options = { upsert: true };
       const updateDoc = {
-        $set:user
-      }
+        $set: user,
+      };
       const result = await usersCollection.updateOne(query, updateDoc, options);
       console.log(result);
       res.send(result);
