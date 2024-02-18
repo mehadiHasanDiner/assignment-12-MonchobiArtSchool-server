501179775aa89d835a26b6ec0a094a3266460115
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2024-02-19 00:15:32 +0600

    user make an admin patch api done

--- index.js
+++ index.js
@@ -111,6 +111,19 @@ async function run() {
       res.send(result);
     });
 
+    // make user role as an admin
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
