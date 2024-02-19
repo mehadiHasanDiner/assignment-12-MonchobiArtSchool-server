f0eb18eec19b5656dc0a4ddd11839267cb6ef790
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2024-02-20 01:55:40 +0600

    some update of api's

--- index.js
+++ index.js
@@ -107,6 +107,14 @@ async function run() {
       res.send(result);
     });
 
+    // get user and his role
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await usersCollection.findOne(query);
+      res.send(result);
+    });
+
     app.get("/users", async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
