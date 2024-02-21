acdf57ff16186a1e23ca33daecd4f48bf605dcad
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2024-02-22 01:50:41 +0600

    all instructor posted data get api done

--- index.js
+++ index.js
@@ -133,6 +133,12 @@ async function run() {
       res.send(result);
     });
 
+    // get all classes data posted by instructor
+    app.get("/allClasses", async (req, res) => {
+      const result = await newClassesCollection.find().toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
